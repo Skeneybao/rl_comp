e892614306eb66661a5831f6d2a1a81f6147bf08
xinyuany <xinyuany@opera.com>
2023-12-18 15:03:10 +0800

    update run_actor test

--- training/DQN/tests/test_actor.py
+++ training/DQN/tests/test_actor.py
@@ -1,5 +1,5 @@
 import unittest
-import env
+
 from env.chooseenv import make
 from training.DQN.actor import run_actor
 from training.DQN.model import Actor, ActorConfig, ModelIOWrapper
@@ -10,15 +10,15 @@ from training.reward.dummy_reward import cal_reward as dummy_reward
 class ActorTestCase(unittest.TestCase):
     game = make('kafang_stock', seed=None)
 
-    def test_something(self):
+    def test_run_actor(self):
         model = Actor(ActorConfig(28, 64, 11))
         model_io_wrapper = ModelIOWrapper(model)
         replay_buffer = ReplayBuffer(1024)
         run_actor(self.game, model_io_wrapper, replay_buffer, dummy_reward)
 
-        self.assertEqual(True, True)
+        self.assertTrue(0 < len(replay_buffer.memory) <= 1024)
+        self.assertTrue(len([1 for _, _, _, _, done in replay_buffer.memory if done != 0]) == 1)
 
 
 if __name__ == '__main__':
-    # unittest.main()
-    pass
+    unittest.main()
