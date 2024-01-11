fe74750391d7c51686f5b12178201a10899d4823
xinyuany <xinyuany@opera.com>
2024-01-11 15:14:04 +0800

    fix hidden dim definition

--- training/model/DNN.py
+++ training/model/DNN.py
@@ -26,8 +26,9 @@ class DNN(nn.Module):
     ):
         super().__init__()
         self.fc_in = nn.Linear(input_dim, hidden_dim[0])
-        self.fcs = [nn.Linear(hidden_dim[i], hidden_dim[i + 1])
-                    for i in range(len(hidden_dim) - 1)]
+        self.hidden = nn.ModuleList()
+        for i in range(len(hidden_dim) - 1):
+            self.hidden.append(nn.Linear(hidden_dim[i], hidden_dim[i + 1]))
         self.fc_out = nn.Linear(hidden_dim[-1], output_dim)
 
         self.activation = activations[activation]
@@ -38,8 +39,8 @@ class DNN(nn.Module):
 
     def forward(self, x):
         x = self.activation(self.fc_in(x))
-        for fc in self.fcs:
-            x = self.activation(fc(x))
+        for layer in self.hidden:
+            x = self.activation(layer(x))
         x = self.fc_out(x)
         if self.output_activation is not None:
             x = self.output_activation(x)
