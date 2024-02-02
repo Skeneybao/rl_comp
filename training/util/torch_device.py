05d3983209f7bc69c2a923e3cc5b490d9bb90c5d
xinyuany <xinyuany@opera.com>
2024-02-02 11:36:28 +0800

    Revert "update auto_get_device to not use weak gpu"
    This reverts commit 1ed87d225c05aec73537295e805f53b2916cd185.

--- training/util/torch_device.py
+++ training/util/torch_device.py
@@ -3,12 +3,6 @@ import torch
 
 def auto_get_device():
     if torch.cuda.is_available():
-        # get the gpu's name
-        gpu_name = torch.cuda.get_device_name(0)
-        # if device contains 1030, use cpu
-        if '1030' in gpu_name:
-            return torch.device('cpu')
-        else:
-            return torch.device('cuda')
+        return torch.device('cuda')
     else:
         return torch.device('cpu')
