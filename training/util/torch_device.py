1ed87d225c05aec73537295e805f53b2916cd185
xinyuany <xinyuany@opera.com>
2024-02-02 11:22:01 +0800

    update auto_get_device to not use weak gpu

--- training/util/torch_device.py
+++ training/util/torch_device.py
@@ -3,6 +3,12 @@ import torch
 
 def auto_get_device():
     if torch.cuda.is_available():
-        return torch.device('cuda')
+        # get the gpu's name
+        gpu_name = torch.cuda.get_device_name(0)
+        # if device contains 1030, use cpu
+        if '1030' in gpu_name:
+            return torch.device('cpu')
+        else:
+            return torch.device('cuda')
     else:
         return torch.device('cpu')
