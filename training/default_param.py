96a07c0d679c889c1b946d15cd756e16b344d024
Xinyuan Yang <37404326+Yxang@users.noreply.github.com>
2024-02-17 18:46:36 +0800

    Merge exp (#50)
    * add feature engine 4 in getter
    
    * init exp config
    
    * fix engine type
    
    * add exp config on l2 reg
    
    * fix twap90
    
    * fix twap90
    
    * fix 90
    
    * fix 90
    
    * fix l2reg data type
    
    * fix l2reg data type
    
    * exp concurrency
    
    * update lr
    
    * update lr

--- training/default_param.py
+++ training/default_param.py
@@ -5,20 +5,20 @@ default_param = {
 
     ## env param
     'env$mode': 'ordered',
-    'env$reward_fn': 'normalized_net_return',
+    'env$reward_fn': 'single600T',
 
     ## feature engine param
-    'feature_engine_type': 'version3Simple',
+    'feature_engine_type': 'version4',
     'feature_engine$max_position': 10,
     # 'feature_engine$sample_param': '1'
 
     ## replay buffer
-    'replay_buffer_type': 'PrioritizedReplayBuffer',
+    'replay_buffer_type': 'ReplayBuffer',
     'replay_buffer$capacity': 50000,
 
     ## model param
-    'model_type': 'full_pos_dnn',
-    'model$hidden_dim': [32, 32],
+    'model_type': 'dnn',
+    'model$hidden_dim': [16, 16, 16],
 
     ## output wrapper param
     'output_wrapper_type': 'action3',
@@ -33,7 +33,7 @@ default_param = {
     'learner_config$batch_size': 512,
     'learner_config$gamma': 0.99,
     'learner_config$tau': 0.005,
-    'learner_config$lr': 5e-8,
+    'learner_config$lr': 5e-7,
     'learner_config$optimizer_type': 'AdamW',
     'learner_config$model_save_step': 20000,
     'learner_config$minimal_buffer_size': 2000,
